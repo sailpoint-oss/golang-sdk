5070c6e58d39e1a5417c5256ef1949909daff15e
tyler-mairose-sp <tyler-mairose-sp@users.noreply.github.com>
2024-08-06 20:44:26 +0000

    Bump version to v2.1.8

--- api_v2024/client.go
+++ api_v2024/client.go
@@ -650,7 +650,7 @@ func (c *APIClient) prepareRequest(
 
 	// Add the user agent to the request.
 	localVarRequest.Header.Add("User-Agent", c.cfg.UserAgent)
-localVarRequest.Header.Add("X-SailPoint-SDK", "2.0.7")
+localVarRequest.Header.Add("X-SailPoint-SDK", "2.1.8")
 
 	if ctx != nil {
 		// add context to the request
--- api_v3/configuration.go
+++ api_v3/configuration.go
@@ -101,7 +101,7 @@ ClientId: 		  clientId,
 		TokenURL:		  tokenURL,
 		Token:			  token,
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "OpenAPI-Generator/2.0.8/go",
+		UserAgent:        "OpenAPI-Generator/2.1.8/go",
 		Debug:            false,
 		Experimental:	  experimental,
 		Servers:          ServerConfigurations{
