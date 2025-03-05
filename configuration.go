045844d7a7fd54d86b0159d3e91cb38b7f6d2c67
darrell-thobe-sp <darrell-thobe-sp@users.noreply.github.com>
2025-03-05 13:11:13 +0000

    Bump version to v2.1.20

--- configuration.go
+++ configuration.go
@@ -77,7 +77,7 @@ type Configuration struct {
 func NewConfiguration(clientConfiguration ClientConfiguration) *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:       make(map[string]string),
-		UserAgent:           "OpenAPI-Generator/2.1.19/go",
+		UserAgent:           "OpenAPI-Generator/2.1.20/go",
 		Debug:               false,
 		ClientConfiguration: clientConfiguration,
 	}
@@ -88,7 +88,7 @@ func NewConfiguration(clientConfiguration ClientConfiguration) *Configuration {
 func NewCLIConfiguration(clientConfiguration ClientConfiguration) *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:       make(map[string]string),
-		UserAgent:           "SailPoint-CLI/2.1.19/go",
+		UserAgent:           "SailPoint-CLI/2.1.20/go",
 		Debug:               false,
 		ClientConfiguration: clientConfiguration,
 	}
