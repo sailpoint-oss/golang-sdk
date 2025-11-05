7908bd3e71fce0afea75c27d9c78736fa59689bd
philip-ellis-sp <philip-ellis-sp@users.noreply.github.com>
2025-11-05 17:05:24 +0000

    Bump version to v2.5.0

--- api_v2026/model_json_patch_operation_value.go
+++ api_v2026/model_json_patch_operation_value.go
@@ -12,8 +12,8 @@ package api_v2026
 
 import (
 	"encoding/json"
-	"fmt"
 	"gopkg.in/validator.v2"
+	"fmt"
 )
 
 // JsonPatchOperationValue - The value to be used for the operation, required for \"add\" and \"replace\" operations
