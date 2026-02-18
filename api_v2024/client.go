49ac3e70a43e02faeae148524680d421c32f03aa
developer-relations-sp <123671052+developer-relations-sp@users.noreply.github.com>
2026-02-18 22:07:08 +0000

    Bump version to v2.7.5

--- api_v2024/client.go
+++ api_v2024/client.go
@@ -698,7 +698,7 @@ func (c *APIClient) prepareRequest(
 
 	// Add the user agent to the request.
 	localVarRequest.Header.Add("User-Agent", c.cfg.UserAgent)
-localVarRequest.Header.Add("X-SailPoint-SDK", "2.7.4")
+localVarRequest.Header.Add("X-SailPoint-SDK", "2.7.5")
 
 	if ctx != nil {
 		// add context to the request
