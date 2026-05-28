12d481248adf8a6efe5ad1698451bb1b6e800767
luke-hagar-sp <193985956+luke-hagar-sp@users.noreply.github.com>
2026-05-28 09:24:07 +0000

    Bump version to v2.7.86

--- api_v2025/client.go
+++ api_v2025/client.go
@@ -728,7 +728,7 @@ func (c *APIClient) prepareRequest(
 	}
 
 	// Add the user agent to the request.
-	userAgent := "SailPoint-SDK-Go/2.7.85"
+	userAgent := "SailPoint-SDK-Go/2.7.86"
 	if c.cfg.UserAgent != "" {
 		userAgent += " " + c.cfg.UserAgent
 	}
