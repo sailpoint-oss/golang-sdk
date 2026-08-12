8ac075abbcee388a323b33bf9250a03e486c782a
luke-hagar-sp <193985956+luke-hagar-sp@users.noreply.github.com>
2026-08-12 08:38:19 +0000

    Bump version to v3.1.17

--- nerm_v2025/client.go
+++ nerm_v2025/client.go
@@ -422,7 +422,7 @@ func (c *APIClient) prepareRequest(
 	}
 
 	// Add the user agent to the request.
-	userAgent := "SailPoint-SDK-Go/3.1.16"
+	userAgent := "SailPoint-SDK-Go/3.1.17"
 	if c.cfg.UserAgent != "" {
 		userAgent += " " + c.cfg.UserAgent
 	}
